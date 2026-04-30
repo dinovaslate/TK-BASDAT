6fe512d473e07a5d9d6c3383412f6ce35a820887
Ega Sudjali <egasudjali1989@gmail.com>
2026-04-30 23:24:16 +0700

    add feature section-3-claim-transfer-operations

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import { AppProvider, useAppContext } from './context/AppContext';
 import AdminLayout from './layouts/AdminLayout';
 import MemberLayout from './layouts/MemberLayout';
 import AdminDashboardPage from './pages/admin/AdminDashboardPage';
+import ClaimsPage from './pages/admin/ClaimsPage';
+import MembersPage from './pages/admin/MembersPage';
+import ReportsPage from './pages/admin/ReportsPage';
+import StaffPage from './pages/admin/StaffPage';
+import TransactionsPage from './pages/admin/TransactionsPage';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
+import BuyMilesPage from './pages/member/BuyMilesPage';
+import ClaimPage from './pages/member/ClaimPage';
+import IdentityPage from './pages/member/IdentityPage';
 import MemberDashboardPage from './pages/member/MemberDashboardPage';
+import RewardsPage from './pages/member/RewardsPage';
+import TransferMilesPage from './pages/member/TransferMilesPage';
 
 function AppRoutes() {
   const { toasts, removeToast } = useAppContext();
@@ -20,11 +30,21 @@ function AppRoutes() {
           <Route path="/login" element={<LoginPage />} />
 
           <Route element={<MemberLayout />}>
+            <Route path="/member/buy-miles" element={<BuyMilesPage />} />
             <Route path="/member/dashboard" element={<MemberDashboardPage />} />
+            <Route path="/member/claim" element={<ClaimPage />} />
+            <Route path="/member/identity" element={<IdentityPage />} />
+            <Route path="/member/rewards" element={<RewardsPage />} />
+            <Route path="/member/transfer" element={<TransferMilesPage />} />
           </Route>
 
           <Route element={<AdminLayout />}>
             <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+            <Route path="/admin/claims" element={<ClaimsPage />} />
+            <Route path="/admin/members" element={<MembersPage />} />
+            <Route path="/admin/reports" element={<ReportsPage />} />
+            <Route path="/admin/staff" element={<StaffPage />} />
+            <Route path="/admin/transactions" element={<TransactionsPage />} />
           </Route>
 
           <Route path="*" element={<Navigate to="/" replace />} />
